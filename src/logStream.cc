825e818ee68250420ebf60b82e1f3062bee66355
Mark Essel <messel@gmail.com>
2012-07-05 17:59:24 -0400

    moved clock source into logStream.cc

--- src/logStream.cc
+++ src/logStream.cc
@@ -5,3 +5,61 @@ void LogStream::MakeNodePackageManagerHappy() {
     cout << existence << endl;
 }
 
+string RunTime::TimeFormat(const double NumSeconds) const
+{
+    stringstream line;
+    const long BSIZE = 100;
+    char buf[BSIZE];
+    if (NumSeconds > 1.0) {
+        double total_seconds = NumSeconds;
+        long minutes = (long)total_seconds / 60;
+        long hours = minutes / 60;
+        minutes = minutes - hours * 60;
+        double seconds = total_seconds - minutes * 60 - hours * 3600;
+        if (hours) sprintf(buf,"%ld:%2.2ld:%5.3f",hours,minutes,seconds);
+        else if (minutes) sprintf(buf,"%2.2ld:%5.3f",minutes,seconds);
+        else sprintf(buf,"%5.3f seconds",seconds);
+    }
+    else {
+        double milliSeconds = NumSeconds * 1000.0;
+        if (milliSeconds > 1.0) {
+            sprintf(buf,"%5.3f ms",milliSeconds);
+        }
+        else {
+            double microSeconds = NumSeconds * 1e6;
+            if (microSeconds > 1.0) {
+                sprintf(buf,"%5.3f us",microSeconds);
+            }
+            else {
+                double nanoSeconds = NumSeconds * 1e9;
+                sprintf(buf,"%5.3f ns",nanoSeconds);
+            }
+        }
+    }
+    return buf;
+}
+
+double RunTime::TotalSeconds() const
+{
+    timeval val;
+    gettimeofday(&val,NULL);
+    double dSec = val.tv_sec - m_first.tv_sec;
+    double dMicroSec = val.tv_usec - m_first.tv_usec;
+    return (dSec + (dMicroSec/1e6) );
+}
+
+double RunTime::TotalMilliSeconds() const
+{
+    return (TotalSeconds() * 1000);
+}
+
+double RunTime::TotalMicroSeconds() const
+{
+    return (TotalSeconds() * 1e6);
+}
+
+ostream& operator<<( ostream& ros, const RunTime &rTime) 
+{
+    ros << rTime.TimeFormat(rTime.TotalSeconds());
+    return ros;
+}
