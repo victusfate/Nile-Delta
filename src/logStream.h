1ad0eb7f82436f29db7b07682b1b5e56b8d04e2e
Mark Essel <messel@gmail.com>
2013-02-22 15:54:30 -0500

    shutting up Nile-Delta

--- src/logStream.h
+++ src/logStream.h
@@ -22,7 +22,7 @@ using namespace std;
 using namespace std::tr1;
 using namespace v8;
 
-const bool VERBOSE_LOGGING = true;
+const bool VERBOSE_LOGGING = false;
 
 enum LogBlobType { LBMAP, LBINT, LBDOUBLE, LBSTRING };
 
